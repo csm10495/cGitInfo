0b7056b4cfa1e4ec7d25051f101a995e7eac5b05
Charles Machalow <csm10495@gmail.com>
2018-11-14 12:12:53 -0800

    Fix HEAD oddity

--- c_git_info.py
+++ c_git_info.py
@@ -58,8 +58,10 @@ def getCurrentBranch():
     Brief:
         Returns the name of the current branch
     '''
-    output = subprocess.check_output('git log -n 1 --pretty=%d HEAD', shell=True).decode()
-    output = output.split(',')[-1].split(')')[0]
+    output = subprocess.check_output('git rev-parse --abbrev-ref HEAD', shell=True).decode().strip()
+    if output == 'HEAD':
+        output = subprocess.check_output('git log -n 1 --pretty=%d HEAD', shell=True).decode()
+        output = output.split(',')[-1].split(')')[0].split('/', 1)[-1]
     return output.strip()
 
 def getListOfCommits(branch=None):
